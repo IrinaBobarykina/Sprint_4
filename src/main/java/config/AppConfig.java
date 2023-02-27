8db0ffd3ed5abebfa6b63475c87bdaaa844b2dd7
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-02-27 22:33:23 +0300

    Delete order page url from file

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -2,6 +2,4 @@ package config;
 
 public class AppConfig {
     public static final String MAIN_PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
-    public static final String ORDER_PAGE_URL = "https://qa-scooter.praktikum-services.ru/order/";
-
 }
