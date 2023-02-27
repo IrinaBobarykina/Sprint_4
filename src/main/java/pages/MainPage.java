c2a3977533929f181f8c53126ebbbf2633da69fc
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-02-27 14:27:25 +0300

    Add locators in MainPage.java

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,4 +1,16 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+
 public class MainPage {
+    WebDriver driver;
+    //Кнопка "Заказать" в хедере
+    private final By btnOrderInTheHeader = By.xpath(".//div[@class='Header_Nav__AGCXC']/button[@class='Button_Button__ra12g']");
+    //Кнопка "Заказать" в середине
+    private final By btnOrderInTheMiddle = By.className("Button_Middle__1CSJM");
+    //Вопрос о важном
+    private final By accordionQuestion = By.className("accordion__item");
+    //Ответ на вопрос
+    private final By accordionAnswer = By.className("accordion__panel");
 }
---
+++ src/test/java/OrderScooterTest.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+
+public class FirstTest {
+//Нажать кнопку заказать (одну из)
+//Вбить имя
+//Вбить фамиилию
+    //Вбить адрес
+    //Выбрать метро
+    //Вбить телефон
+    //Ввбрать дату
+    //Выбрать срок
+    //Выбрать цвет
+    //Оставить комментарий
+    //Нажать заказать
+}
