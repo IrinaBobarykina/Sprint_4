6364429fda6a839d61ad3637a8f738a5594ff40c
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-02-27 15:04:37 +0300

    Add methods to Main.java

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class MainPage {
     WebDriver driver;
@@ -9,8 +10,25 @@ public class MainPage {
     private final By btnOrderInTheHeader = By.xpath(".//div[@class='Header_Nav__AGCXC']/button[@class='Button_Button__ra12g']");
     //Кнопка "Заказать" в середине
     private final By btnOrderInTheMiddle = By.className("Button_Middle__1CSJM");
-    //Вопрос о важном
+    //Вопрос о важном ???
     private final By accordionQuestion = By.className("accordion__item");
-    //Ответ на вопрос
+    //Ответ на вопрос ???
     private final By accordionAnswer = By.className("accordion__panel");
+
+    public MainPage (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickOrderInTheHeaderButton() {
+        driver.findElement(btnOrderInTheHeader).click();
+    }
+
+    public void clickOrderInTheMiddleButton() {
+        driver.findElement(btnOrderInTheMiddle).click();
+    }
+
+    public void clickQuestion(String question) {
+        driver.findElement(By.xpath(".//div[@class='accordion__heading']/div[text() =" + question + "]")).click();
+    }
+
 }
