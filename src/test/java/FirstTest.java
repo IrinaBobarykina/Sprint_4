87f1c7174dd2efe250475f02feee48b49f2cae61
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-02-27 11:39:59 +0300

    Add imports

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class FirstTest {
+
+}
