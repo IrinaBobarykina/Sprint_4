b9d7ef33abb39e45a78d7364467e552719424f5c
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-02-27 20:46:45 +0300

    Update AppConfig

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -3,4 +3,5 @@ package config;
 public class AppConfig {
     public static final String MAIN_PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
     public static final String ORDER_PAGE_URL = "https://qa-scooter.praktikum-services.ru/order/";
+
 }
